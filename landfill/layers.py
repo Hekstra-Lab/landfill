a39dfb19a4b0f91318690be8764dcea387ddc9de
John Russell <johncrussell25@gmail.com>
2021-06-25 11:07:14 -0400

    positional encoding layer

--- landfill/layers.py
+++ landfill/layers.py
@@ -1,3 +1,4 @@
+from numbers import Number
 import torch
 from torch import nn
 
@@ -17,4 +18,23 @@ class GaussianFourierFeatures(nn.Module):
         y = x.mm(self.B)
         c = torch.cos(y)
         s = torch.sin(y)
+        return torch.cat((c,s),dim=-1)
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, n_dims, n_features, log_min, log_max, rand=False):
+        super().__init__()
+        self.n_features = n_features
+        self.n_dims =n_dims
+        if rand:
+            B = 2**((log_max-log_min)*torch.rand(self.n_features)+log_min)
+        else:
+            B = 2**torch.linspace(log_min, log_max, self.n_features)
+           
+        self.B = nn.Parameter(B.unsqueeze(-1).expand(-1,n_dims).t(), requires_grad=False)
+    
+    def forward(self, x):
+        y =x[:,:,None]*self.B[None, None, :]
+        y = y.reshape(x.shape[0], -1)
+        c = torch.cos(2*np.pi*y)
+        s = torch.sin(2*np.pi*y)
         return torch.cat((c,s),dim=-1)
