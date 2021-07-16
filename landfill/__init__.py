719fcaaf9cb73607de7896fd30ec6a51866906f3
John Russell <johncrussell25@gmail.com>
2021-07-16 15:26:45 -0400

    update __init__ to reflect torch module structure

--- landfill/__init__.py
+++ landfill/__init__.py
@@ -1,4 +1,7 @@
 # Must import __version__ first to avoid errors importing this file during the build process.
 # See https://github.com/pypa/setuptools/issues/1724#issuecomment-627241822
 from ._version import __version__
-from .landfill import *
+# from .landfill import *
+from . import layers
+from . import model
+
